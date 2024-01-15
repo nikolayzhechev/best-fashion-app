7b148912cb1c0f0d8c5c5964f9cdbcdc5c79cd0e
Nikolay Zhechev <nikolayzhechev@yahoocom>
2024-01-15 21:39:57 +0200

    Update/ improve db config

--- data-scraper/scraper/db/dbConfig.js
+++ data-scraper/scraper/db/dbConfig.js
@@ -2,14 +2,14 @@ import { client } from "./db.js";
 
 const dbName = "BFA";
 const db = client.db(dbName);
-const storesCollection = "Stores";
-const queryCollection = "Queries";
-const retreivedDataCollection = "RetreivedData";
+const allCollections = {
+    storesCollection: "Stores",
+    queryCollection: "Queries",
+    retreivedDataCollection: "RetreivedData"
+}
 
 export default {
     db,
     dbName,
-    storesCollection,
-    queryCollection,
-    retreivedDataCollection
+    allCollections
 }
