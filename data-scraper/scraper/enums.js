edbc1f8be88aad348b65869c3e95b3318550253e
Nikolay Zhechev <nikolayzhechev@yahoo.com>
2025-12-02 11:05:49 +0200

    CLeanup repo

--- data-scraper/scraper/enums.js
+++
@@ -1,8 +0,0 @@
-const siteEnums = Object.freeze({
-    aboutYou: "aboutyou",
-    zara: "zara",
-    remixShop: "remixshop",
-    fashionDays: "fashiondays"
-});
-
-export default siteEnums;
--- data-scraper/scraper/executeScrape.js
+++
@@ -1,5 +0,0 @@
-import * as dataHandle from "./scraper.js";
-
-export async function executeScrape (siteName, type, queryData, appendToExisting) {
-    return await dataHandle.scrapeDynamicData(siteName, type, queryData, appendToExisting);
-}
